2e0a576c9e45fe2fa910500499914bdff8e7ddb5
DJ Drakos <dianajodrakos@gmail.com>
2021-09-24 12:00:55 -0700

    wrote POST test, updated import syntax

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,9 +3,29 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
+
+
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('POSTS a new entry', async () => {
+    const entry = {
+      name: 'DJ',
+      event: true,
+      note: ''
+    };
+
+    const res = await request(app)
+      .post('/api/v1/alchemy-cry-lab')
+      .send(entry);
+
+    expect(res.body).toEqual({
+      id: '1',
+      date: 'currentData',
+      ...entry,
+    });
   });
 
   afterAll(() => {
