2be65db9b9ef94490f5cffa006e314a98d72bbe3
DJ Drakos <dianajodrakos@gmail.com>
2021-09-24 12:13:40 -0700

    .get route naming bug

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,6 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-
-
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
